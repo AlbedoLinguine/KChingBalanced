8172a712df2ac687f4dd563bea9e65d76787d2f3
AlbedoLinguine <yogevcr@gmail.com>
2024-10-09 20:40:55 -0500

    init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,2 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-    readonly VITE_EMAILJS_SERVICE_ID: string
-    readonly VITE_EMAILJS_TEMPLATE_ID: string
-    readonly VITE_EMAILJS_USER_ID: string
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv
-  }
